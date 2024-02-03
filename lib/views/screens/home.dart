39e9d32dc8c368b51f0b8432f215089ecc64d8da
Saurabh <76562057+sbisht01-dev@users.noreply.github.com>
2024-02-04 02:00:03 +0530

    keyboard dismiss

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -8,57 +8,62 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
         backgroundColor: Colors.white,
-        title: const CustomAppBar(),
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(children: [
-          const Searchbar(),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: Category.cHeight,
-              width: MediaQuery.of(context).size.width,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: ((context, index) => const Category()),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          title: const CustomAppBar(),
+        ),
+        body: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          physics: const BouncingScrollPhysics(),
+          child: Column(children: [
+            const Searchbar(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: Category.cHeight,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: ((context, index) => const Category()),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              // width: MediaQuery.of(context).size.width,
-              child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 16,
-                      crossAxisSpacing: 16,
-                      mainAxisExtent: 300),
-                  itemCount: 16,
-                  itemBuilder: (context, index) => ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 230, 172, 47),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                // width: MediaQuery.of(context).size.width,
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 16,
+                            crossAxisSpacing: 16,
+                            mainAxisExtent: 300),
+                    itemCount: 16,
+                    itemBuilder: (context, index) => ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color.fromARGB(255, 230, 172, 47),
+                            ),
+                            child: Image.network(
+                              "https://w0.peakpx.com/wallpaper/736/818/HD-wallpaper-demon-slayer-anime-sword.jpg",
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                          child: Image.network(
-                            "https://w0.peakpx.com/wallpaper/736/818/HD-wallpaper-demon-slayer-anime-sword.jpg",
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )),
+                        )),
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
